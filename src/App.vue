27f4d90ca62a3ca3913090e04fb97eadf9ecaead
websmiths <websmiths@users.noreply.github.com>
2025-09-01 06:37:24 +0000

    deploy: 14a78f718dd1c5fcc000d127a4571225f2e9e069

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<script setup>
+import { shallowRef } from 'vue'
+import { useFlowStore } from '@/stores/flow.js'
+const flowStore = useFlowStore()
+
+import FlowChat from '@/components/FlowChat.vue'
+import Suggestions from '@/components/Suggestions.vue'
+
+/*
+ * Todo:
+ *
+ * Store session ID for later retrieval
+ *
+ * */
+
+const startupPrompts = [
+  'What’s best for cleaning oil from sandstone?',
+  'What is the best product for cleaning kitchen floors',
+  'Where can I buy Agar products?',
+  'Tell me about Oil Cleaning products',
+]
+
+const selectedProduct = shallowRef(null)
+
+const handleProductSelection = productKey => {
+  selectedProduct.value = productKey
+}
+</script>
+
+<template>
+  <div id="ask-agar-wrapper">
+    <div
+      class="chat-interface"
+      :class="{ 'chat-expanded': flowStore.chatExpanded }"
+    >
+      <div class="close-chat">
+        <button
+          class="btn-close"
+          @click="flowStore.chatExpanded = false"
+        />
+      </div>
+      <FlowChat
+        class="chat"
+        submit-label="Ask Agar"
+        :startup-prompts
+      />
+      <Suggestions class="suggestions" />
+    </div>
+  </div>
+  <!--  <pre>{{ flowStore.flowState }}</pre>-->
+</template>
+
+<style lang="scss">
+#ask-agar-wrapper {
+  &:has(.chat-expanded) {
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 2;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+
+  .btn {
+    height: unset !important;
+  }
+
+
+  .chat-interface {
+    display: flex;
+    gap: 0;
+    height: 3rem;
+    top: 0;
+    border-radius: 0.3em;
+
+    // to reset some agar stuff
+    text-align: left;
+
+    //transition: height 0.6s ease-in-out;
+    transition: all 0.6s ease-in-out;
+
+    position: relative;
+
+    .close-chat {
+      position: absolute;
+      right: 0;
+      top: -2rem;
+    }
+
+    &.chat-expanded {
+      height: 80vh;
+      width: 80vw;
+      margin: 0 auto;
+      background: #fff;
+      padding: 1rem;
+      gap: 1rem;
+      top: 3rem;
+    }
+
+    .suggestions,
+    .close-chat,
+    .conversation {
+      transition: all 0.6s ease-in-out;
+    }
+
+    &:not(.chat-expanded) {
+      .suggestions,
+      .close-chat,
+      .conversation {
+        opacity: 0;
+        //height: 0;
+        margin: 0;
+        padding: 0;
+      }
+
+      .suggestions {
+        flex: 0 1 0;
+        padding: 0;
+      }
+    }
+
+    > section {
+      flex: 1 1 auto;
+
+      &.chat {
+        flex-basis: 80%;
+      }
+
+      &.suggestions {
+        flex-basis: 20%;
+        padding: 1rem;
+        border-radius: 0.4em;
+        border: 1px solid var(--bs-primary);
+        background: var(--bs-primary);
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/_ProductSuggestion.vue
@@ -0,0 +1,60 @@
+<script setup>
+const { product } = defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+})
+
+const badgeClass = score => {
+  if (score >= 90) return 'bg-success'
+  if (score >= 70) return 'bg-info'
+  return 'bg-warning'
+}
+</script>
+
+<template>
+  <div class="product-suggestion text-light rounded bg-dark p-3 my-2">
+    <!--      <pre class="text-break">{{ product }}</pre>-->
+    <h6
+      v-if="product.name"
+      class="d-flex justify-content-between align-items-start"
+    >
+      <span>{{ product.name }}</span>
+      <span
+        v-if="product.score"
+        class="badge rounded-pill text-bg-light"
+        :class="badgeClass(product.score)"
+        >{{ product.score }}</span
+      >
+    </h6>
+    <p v-if="product.category">{{ product.category }}</p>
+
+    <p v-if="product.image_url">
+      <img
+        :src="product.image_url"
+        :alt="product.name"
+        class="img-fluid"
+      />
+    </p>
+
+    <p v-if="product.url">
+      <a
+        :href="product.url"
+        target="_blank"
+        class="btn btn-light btn-sm"
+      >
+        <i class="fa-solid fa-link" />
+        Product link</a
+      >
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.product-suggestion {
+  :last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
